372913186677202aad54cb8f421219c1e44f0698
Birgitta B <bboeckel@thoughtworks.com>
2017-04-14 12:05:16 +0200

    While a key is pressed, keep tile a certain color, otherwise switch back to default color

--- main.js
+++ main.js
@@ -4,17 +4,46 @@
 // You can substitute option for alt and command for meta.
 // Other special keys are backspace, tab, enter, return, capslock, esc, escape, space, pageup, pagedown, end, home, left, up, right, down, ins, del, and plus.
 
+var spaceBarLastPressed = undefined;
+var spaceBarWatcher = undefined;
+
+function watchSpaceBar() {
+  if(spaceBarLastPressed !== undefined) {
+    var timeSinceLastPress = Date.now() - spaceBarLastPressed;
+    if(timeSinceLastPress >= 500) {
+      stopToWatchSpaceBar();
+      makeTileGreen();
+    } else {
+      makeTileRed();
+    }
+  }
+}
+
+function stopToWatchSpaceBar() {
+  if(spaceBarWatcher !== undefined) {
+    console.log('STOPPED TO PRESS SPACE BAR');
+    clearInterval(spaceBarWatcher);
+    spaceBarWatcher = undefined;
+  }
+}
+
+function makeTileGreen() {
+  var spaceBarTile = $('#on-space-bar');
+  spaceBarTile.removeClass('red');
+  spaceBarTile.addClass('green');
+}
+
+function makeTileRed() {
+  var spaceBarTile = $('#on-space-bar');
+  spaceBarTile.removeClass('green');
+  spaceBarTile.addClass('red');
+}
+
 Mousetrap.bind('space', function() { 
   console.log('HIT SPACE');
-
-  var spaceBarTile = $('#on-space-bar');
-  var isGreen = spaceBarTile.hasClass('green');
-  if(isGreen) {
-    spaceBarTile.removeClass('green');
-    spaceBarTile.addClass('red');
-  } else {
-    spaceBarTile.removeClass('red');
-    spaceBarTile.addClass('green');
+  spaceBarLastPressed = Date.now();
+  if(spaceBarWatcher === undefined) {
+    console.log('STARTING TO PRESS SPACE BAR');
+    spaceBarWatcher = setInterval(watchSpaceBar, 100);
   }
-  
 });
